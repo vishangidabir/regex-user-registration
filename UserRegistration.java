3132153b4058d8053766addb557a28c49cca09f0
vishangi <vishangid@gmail.com>
2022-06-29 11:38:08 +0530

    Added : user registration system

--- UserRegistration.java
+++ UserRegistration.java
@@ -10,6 +10,5 @@ public class UserRegistration {
 		System.out.println("Enter First Name :");
 		String firstName = scanner.next();
 		System.out.println(Pattern.matches("[A-Z][a-z]{3,}", firstName));
-
 	}
 }
