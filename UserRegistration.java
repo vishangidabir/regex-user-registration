b045c418483962596d3a002fb3ba05fb454c8aa9
vishangi <vishangid@gmail.com>
2022-06-29 12:15:25 +0530

    Added : Check password rule-1 validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -7,7 +7,6 @@ public class UserRegistration {
 	
 	public static void main(String args[]) {
 		Scanner scanner = new Scanner(System.in);
-		
 		System.out.println("Welcome to UserRegistration program using Regex !");
 		System.out.println("Enter First Name :");
 		String firstName = scanner.next();
@@ -18,5 +17,11 @@ public class UserRegistration {
 		System.out.println("Enter Email ID :");
 		String emailid = scanner.next();
 		System.out.println(Pattern.matches("[a-z\\d]{5,}@[a-z]{5}.[a-z]{2,}.[a-z]{2,}", emailid));
+		System.out.println("Enter Mobile Number :");
+		String mobileno = scanner.next();
+		System.out.println(Pattern.matches("[+]91 [6789]\\d{9}", mobileno));
+		System.out.println("Enter Password 1 :");
+		String password1 = scanner.next();
+		System.out.println(Pattern.matches("[a-z]{8,}", password1));
 	}
 }
