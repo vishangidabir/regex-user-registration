31f168d8c1d0b433482118ed00954321f5e9fab0
vishangi <vishangid@gmail.com>
2022-06-29 11:35:15 +0530

    Added : user registration system

---
+++ UserRegistration.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+public class UserRegistration {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Welcome to UserRegistration program using Regex !");
+		System.out.println("Enter First Name :");
+		String firstName = scanner.next();
+		System.out.println(Pattern.matches("[A-Z][a-z]{3,}", firstName));
+
+	}
+}
